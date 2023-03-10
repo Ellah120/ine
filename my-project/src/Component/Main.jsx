8acb6f963c6533299bb63dbfec4566dce9c3c6e4
Nwoke Ogechi Emmanuella <nwokeellah88@gmail.com>
2023-03-10 10:57:04 +0100

    changes

--- my-project/src/Component/Main.jsx
+++ my-project/src/Component/Main.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./HeroSection"
 import Socialize from "./Socialize"
 import Connected from "./Connected"
+import HowItWorks from "./HowItWorks"
 function Main() {
   return (
     <div>
     <HeroSection />
     <Socialize />
     <Connected />
+    <HowItWorks />
     </div>
   )
 }
--- my-project/tailwind.config.cjs
+++ my-project/tailwind.config.cjs
@@ -11,6 +11,8 @@ module.exports = {
         "button-color": "rgba(138, 1, 215, 1)",
         "dark-green": "rgba(21, 240, 150, 1)",
       },
+      backgroundImage: { 
+        background: "url('../assets/images/red')" },
     },
   },
   plugins: [],
